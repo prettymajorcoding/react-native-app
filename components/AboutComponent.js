747f02ff4fed80080bd748e3f91a7ca71446c73f
Bjon Byrams <bjonbyrams@gmail.com>
2021-12-22 18:40:43 -0500

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
-import { FlatList } from 'react-native';
-import {PARTNERS} from '../shared/partners.js'
+import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission() {
     return (
@@ -15,10 +22,7 @@ function Mission() {
 }
 
 class About extends Component {
-    constructor(props){
-        super(props);
-        this.state = {partners: PARTNERS}
-    }
+    
     static navigationOptions = {
         title: 'About Us'
     }
@@ -29,17 +33,40 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
+        
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
 
         return (
             <ScrollView> 
                 <Mission />
                 <Card title="Community Partners">
-                    <FlatList
-                        data={this.state.partners}
+                <FlatList 
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -49,4 +76,4 @@ class About extends Component {
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,16 @@ import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions,
+    fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -199,6 +209,13 @@ const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
 
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
+
     render() {
         return (
             <View
@@ -244,4 +261,4 @@ const styles = StyleSheet.create({
 });
 
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
